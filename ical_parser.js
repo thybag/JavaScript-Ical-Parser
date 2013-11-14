47e794ef84692b5b589e3d46cc86e8d43622dabe
j08lue <j08lue@gmail.com>
2013-11-14 14:01:37 +0100

    added function to get past dates and UTC date support

--- ical_parser.js
+++ ical_parser.js
@@ -40,7 +40,7 @@ function ical_parser(feed_url, callback){
 	 */
 	this.makeDate = function(ical_date){
 		//break date apart
-		var dt =  {
+                var dtutc =  {
 			year: ical_date.substr(0,4),
 			month: ical_date.substr(4,2),
 			day: ical_date.substr(6,2),
@@ -48,9 +48,19 @@ function ical_parser(feed_url, callback){
 			minute: ical_date.substr(11,2)
 		}
 		//Create JS date (months start at 0 in JS - don't ask)
-		dt.date = new Date(dt.year, (dt.month-1), dt.day, dt.hour, dt.minute);
+                var utcdatems = Date.UTC(dtutc.year, (dtutc.month-1), dtutc.day, dtutc.hour, dtutc.minute);
+                var dt = {};
+                dt.date = new Date(utcdatems);
+                
+                dt.year = dt.date.getFullYear();
+                dt.month = ('0' + (dt.date.getMonth()+1)).slice(-2);
+                dt.day = ('0' + dt.date.getDate()).slice(-2);
+                dt.hour = ('0' + dt.date.getHours()).slice(-2);
+                dt.minute = ('0' + dt.date.getMinutes()).slice(-2);
+
 		//Get the full name of the given day
 		dt.dayname =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dt.date.getDay()];
+                dt.monthname = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ][dt.date.getMonth()] ;
 		
 		return dt;
 	}
@@ -101,6 +111,7 @@ function ical_parser(feed_url, callback){
 					cur_event.start_time = dt.hour+':'+dt.minute;
 					cur_event.start_date = dt.day+'/'+dt.month+'/'+dt.year;
 					cur_event.day = dt.dayname;
+                                        cur_event.start_date_long = dt.day+'. '+dt.monthname+' '+dt.year ;
 				}
 				//If the type is an end date, do the same as above
 				if(type =='DTEND'){
@@ -113,7 +124,7 @@ function ical_parser(feed_url, callback){
 				}
 				//Convert timestamp
 				if(type =='DTSTAMP') val = this.makeDate(val).date;
-				
+
 				//Add the value to our event object.
 				cur_event[type] = val;
 			}
@@ -157,6 +168,22 @@ function ical_parser(feed_url, callback){
 			if(itm.DTSTART > current_date) future_events.push(itm);
 		});
 		return future_events;
+	}
+	
+	/**
+	 * getPastEvents
+	 * return all events sheduled to take place before the current date.
+	 *
+	 * @return list of events objects
+	 */
+	this.getPastEvents = function(){
+		var past_events = [], current_date = new Date();
+		
+		this.events.forEach(function(itm){
+			//If the event starts before the current time, add it to the array to return.
+			if(itm.DTSTART <= current_date) past_events.push(itm);
+		});
+		return past_events.reverse();
 	}
 	
 	/**
